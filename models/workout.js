665271931b97f72ead0788016fc5c6ea336a7a92
Donnahue George <Donnahuegjr@gmail.com>
2021-03-15 19:41:14 -0400

    added excercise schema

--- models/workout.js
+++ models/workout.js
@@ -7,6 +7,31 @@ const workoutSchema = new Schema({
     type: Date,
     default: () => new Date(),
   },
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model('Workout', workoutSchema);
