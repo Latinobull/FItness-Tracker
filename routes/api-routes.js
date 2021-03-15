a0c0418d26bceba72b440804e49e9ed16b3f1e1a
Donnahue George <Donnahuegjr@gmail.com>
2021-03-15 19:46:10 -0400

    got stats page to work! kinda

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,37 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
 
-router.post('/api/workouts', (req, res) => {
-  Workout.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
 router.get('/api/workouts', (req, res) => {
   Workout.find()
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => res.json(err));
+});
+
+router.post('/api/workouts', (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => res.json(err));
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+  Workout.update(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: {
+        exercises: req.body,
+      },
+    }
+  )
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => res.json(err));
+});
+
+router.get('/api/workouts/range', (req, res) => {
+  Workout.find()
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => res.json(err));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
 
 const PORT = process.env.PORT || 8080;
 
@@ -8,7 +7,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan('tiny'));
 app.use(express.static('public'));
 
 mongoose.connect(
