6f1ea58ca96f647008533289463c06d81dc84d09
Donnahue George <Donnahuegjr@gmail.com>
2021-03-15 19:09:48 -0400

    added stats route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,8 @@ module.exports = app => {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
+
+  app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+  });
 };
