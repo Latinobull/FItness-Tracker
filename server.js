52987711739a144678f5fea18a55a2129d241048
Donnahue George <Donnahuegjr@gmail.com>
2021-03-15 19:27:23 -0400

    adding database syntax

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const htmlRouter = require('./routes/html-routes');
 const apiRouter = require('./routes/api-routes');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(morgan('tiny'));
 app.use(express.static('public'));
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
 htmlRouter(app);
 // apiRouter(app);
 app.listen(PORT, () => {
